8b627d3406ca8956eb11aa18491704ed3e84aaf8
Victoria Vlasova <a5glyk@gmail.com>
2024-04-22 21:03:35 +0300

    refactor pom.xml, add methods (1,2)

---
+++ src/main/java/Methods.java
@@ -0,0 +1,18 @@
+public class Methods {
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign(int a, int b) {
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+
+}
