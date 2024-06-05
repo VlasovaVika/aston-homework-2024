11367ec49f4772cb4ba7ede2067278e1ecc63d88
Victoria Vlasova <a5glyk@gmail.com>
2024-06-05 19:17:37 +0300

    add comments for task 1

--- src/main/java/lesson_13/ArrayOfWords.java
+++ src/main/java/lesson_13/ArrayOfWords.java
@@ -6,14 +6,15 @@ public class ArrayOfWords {
     public static void main(String[] args) {
         String[] arrayOfFruits = {"lime", "strawberry", "apple", "Apple", "pear", "mango", "banana",
                 "cherry", "kiwi", "banana", "orange", "peach", "pineapple", "coconut", "grape"};
+        //вывести список уникальных слов:
         Set<String> listOfUniqueNamesOfFruits = new LinkedHashSet<>();
         Collections.addAll(listOfUniqueNamesOfFruits, arrayOfFruits);
         System.out.println(listOfUniqueNamesOfFruits);
-
+        //вывести список всех слов:
         List<String> listOfAllFruits = new ArrayList<>();
         Collections.addAll(listOfAllFruits, arrayOfFruits);
         System.out.println(listOfAllFruits);
-
+        //посчитать сколько раз встречается каждое слово:
         for (int i = 0; i < listOfAllFruits.size(); i++) {
             System.out.println(listOfAllFruits.get(i) + " is repeated "
                     + Collections.frequency(listOfAllFruits, listOfAllFruits.get(i)) + " times");
