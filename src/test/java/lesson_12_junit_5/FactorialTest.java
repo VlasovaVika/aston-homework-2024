b8713deb5c1cdf46744148e862c5a1de28b939bb
Victoria Vlasova <a5glyk@gmail.com>
2024-06-10 22:52:20 +0300

    refactor task with junit

--- src/test/java/lesson_12_junit_5/FactorialTest.java
+++ src/test/java/lesson_12_junit_5/FactorialTest.java
@@ -1,27 +1,32 @@
 package lesson_12_junit_5;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class FactorialTest {
+    ProgramForFactorial programForFactorial;
+
+    @BeforeEach
+    public void setUp() {
+        programForFactorial = new ProgramForFactorial();
+    }
+
     @Test
     @DisplayName("test for factorial of positive number")
     public void testCalculateFactorial1() throws FactorialException {
-        Assertions.assertEquals(120, ProgramForFactorial.calculateFactorial(5));
+        Assertions.assertEquals(120, programForFactorial.calculateFactorial(5));
     }
 
     @Test
     @DisplayName("test for factorial with value of number = 0")
     public void testCalculateFactorial2() throws FactorialException {
-        Assertions.assertEquals(1, ProgramForFactorial.calculateFactorial(0));
+        Assertions.assertEquals(1, programForFactorial.calculateFactorial(0));
     }
 
     @Test
     @DisplayName("test for factorial with value of number = -1")
     public void testCalculateFactorial3() {
         try {
-            ProgramForFactorial.calculateFactorial(-1);
+            programForFactorial.calculateFactorial(-1);
         } catch (FactorialException e) {
             Assertions.assertEquals("Недопустимое значение", e.getMessage());
         }
