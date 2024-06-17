2afa204e0e5ddeb88b656d29c246427088eb0aa2
Victoria Vlasova <a5glyk@gmail.com>
2024-06-17 21:56:58 +0300

    refactor task1

--- src/main/java/lesson_16/OplataUslugPage.java
+++ src/main/java/lesson_16/OplataUslugPage.java
@@ -1,5 +1,6 @@
 package lesson_16;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class OplataUslugPage {
@@ -8,8 +9,59 @@ public class OplataUslugPage {
     public OplataUslugPage(WebDriver driver) {
         this.driver = driver;
     }
-    public String select_now = "//div[@class=\"select__wrapper\"]/button/span";
-    public String phone_id ="connection-phone";
-    public String sum_id = "connection-sum";
-    public String email_id = "connection-emai";
+
+    private String selectNow = "//div[@class=\"select__wrapper\"]/button/span";
+    private String phoneId = "connection-phone";
+    private String sumId = "connection-sum";
+    private String emailId = "connection-email";
+    private String domashnyiInternet = "//ul[@class=\"select__list\"]/li[2]";
+    private String internetPhoneId = "internet-phone";
+    private String rassrochka = "//ul[@class=\"select__list\"]/li[3]";
+    private String scoreInstalmentId = "score-instalment";
+    private String zadolzhennost = "//ul[@class=\"select__list\"]/li[4]";
+    private String scoreArrears = "score-arrears";
+
+    public String getTextVibranoiUslugi() {
+        return driver.findElement(By.xpath(selectNow)).getText();
+    }
+
+    public String getAttributeOfPhone() {
+        return driver.findElement(By.id(phoneId)).getAttribute("placeholder");
+    }
+
+    public String getAttributeSum() {
+        return driver.findElement(By.id(sumId)).getAttribute("placeholder");
+    }
+
+    public String getAttributeEmail() {
+        return driver.findElement(By.id(emailId)).getAttribute("placeholder");
+    }
+
+    public void clickSelectedNowBtn() {
+        driver.findElement(By.xpath(selectNow)).click();
+    }
+
+    public void clickDomashnyiInternetBtn() {
+        driver.findElement(By.xpath(domashnyiInternet)).click();
+    }
+
+    public String getAttributeInternetPhone() {
+        return driver.findElement(By.id(internetPhoneId)).getAttribute("placeholder");
+    }
+
+    public void clickRassrochkaBtn() {
+        driver.findElement(By.xpath(rassrochka)).click();
+    }
+
+    public String getAttributeScoreInstalment() {
+        return driver.findElement(By.id(scoreInstalmentId)).getAttribute("placeholder");
+    }
+
+    public void clickZadolzhennostBtn() {
+        driver.findElement(By.xpath(zadolzhennost)).click();
+    }
+
+    public String getAttributeScoreArrears() {
+        return driver.findElement(By.id(scoreArrears)).getAttribute("placeholder");
+    }
 }
--- src/test/java/lesson_16/OplataUslugTest.java
+++ src/test/java/lesson_16/OplataUslugTest.java
@@ -2,21 +2,75 @@ package lesson_16;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OplataUslugTest extends BaseTest {
     @Test
-    @DisplayName("first")
+    @DisplayName("Uslugi Svyazi")
     public void testUslugSvyazi() {
         OplataUslugPage oplataUslugPage = new OplataUslugPage(driver);
         assertAll(
-        ()->assertEquals("Услуги связи",
-                driver.findElement(By.xpath(oplataUslugPage.select_now)).getText()),
-        ()-> assertEquals("Номер телефона",
-                driver.findElement(By.id(oplataUslugPage.phone_id)).getAttribute("placeholder"))
+                () -> assertEquals("Услуги связи",
+                        oplataUslugPage.getTextVibranoiUslugi()),
+                () -> assertEquals("Номер телефона",
+                        oplataUslugPage.getAttributeOfPhone()),
+                () -> assertEquals("Сумма",
+                        oplataUslugPage.getAttributeSum()),
+                () -> assertEquals("E-mail для отправки чека",
+                        oplataUslugPage.getAttributeEmail())
+        );
+    }
+    @Test
+    @DisplayName("Domashniy Internet")
+    public void testDomashnegoUnterneta(){
+        OplataUslugPage oplataUslugPage = new OplataUslugPage(driver);
+        oplataUslugPage.clickSelectedNowBtn();
+        oplataUslugPage.clickDomashnyiInternetBtn();
+        assertAll(
+                () -> assertEquals("Домашний интернет",
+                       oplataUslugPage.getTextVibranoiUslugi()),
+                () -> assertEquals("Номер абонента",
+                        oplataUslugPage.getAttributeInternetPhone()),
+                () -> assertEquals("Сумма",
+                        oplataUslugPage.getAttributeSum()),
+                () -> assertEquals("E-mail для отправки чека",
+                        oplataUslugPage.getAttributeEmail())
+        );
+    }
+    @Test
+    @DisplayName("Rassrochka")
+    public void testRassrochki(){
+        OplataUslugPage oplataUslugPage = new OplataUslugPage(driver);
+        oplataUslugPage.clickSelectedNowBtn();
+        oplataUslugPage.clickRassrochkaBtn();
+        assertAll(
+                () -> assertEquals("Рассрочка",
+                        oplataUslugPage.getTextVibranoiUslugi()),
+                () -> assertEquals("Номер счета на 44",
+                        oplataUslugPage.getAttributeScoreInstalment()),
+                () -> assertEquals("Сумма",
+                        oplataUslugPage.getAttributeSum()),
+                () -> assertEquals("E-mail для отправки чека",
+                        oplataUslugPage.getAttributeEmail())
+        );
+    }
+    @Test
+    @DisplayName("Zadolzhennost")
+    public void testZadolzhennosti(){
+        OplataUslugPage oplataUslugPage = new OplataUslugPage(driver);
+        oplataUslugPage.clickSelectedNowBtn();
+        oplataUslugPage.clickZadolzhennostBtn();
+        assertAll(
+                () -> assertEquals("Задолженность",
+                        oplataUslugPage.getTextVibranoiUslugi()),
+                () -> assertEquals("Номер счета на 2073",
+                        oplataUslugPage.getAttributeScoreArrears()),
+                () -> assertEquals("Сумма",
+                        oplataUslugPage.getAttributeSum()),
+                () -> assertEquals("E-mail для отправки чека",
+                        oplataUslugPage.getAttributeEmail())
         );
     }
 }
